171e073c1bbe7990974d89cf04cb52a6484e3bc2
shanhaider23 <shan.haider@gmail.com>
2025-02-07 17:55:38 +0100

    feat: added todo

--- app/(routes)/dashboard/_component/CardInfo.jsx
+++ app/(routes)/dashboard/_component/CardInfo.jsx
@@ -26,6 +26,20 @@ function CardInfo({ budgetList }) {
 		<div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
 			{budgetList ? (
 				<>
+					{/* Number of Budgets Card */}
+					<div className="p-6 bg-green-100 dark:bg-green-800 border-l-4 border-green-500 dark:border-green-600 rounded-lg shadow-md flex items-center gap-4 hover:shadow-lg transition-all flex-wrap">
+						<div className="bg-green-500 text-white p-4 rounded-full">
+							<Wallet size={32} />
+						</div>
+						<div>
+							<h2 className="text-gray-700 dark:text-gray-200 font-semibold lg:text-lg  sm:text-sm">
+								No. of Budgets
+							</h2>
+							<h2 className="text-xl font-bold text-gray-900 dark:text-gray-100">
+								{budgetList.length}
+							</h2>
+						</div>
+					</div>
 					{/* Total Budget Card */}
 					<div className="p-6 bg-blue-100 dark:bg-blue-800 border-l-4 border-blue-500 dark:border-blue-600 rounded-lg shadow-md flex items-center gap-4 hover:shadow-lg transition-all flex-wrap ">
 						<div className="bg-blue-500 text-white p-4 rounded-full">
@@ -52,21 +66,6 @@ function CardInfo({ budgetList }) {
 							</h2>
 							<h2 className="text-xl font-bold text-gray-900 dark:text-gray-100">
 								{totalSpend}
-							</h2>
-						</div>
-					</div>
-
-					{/* Number of Budgets Card */}
-					<div className="p-6 bg-green-100 dark:bg-green-800 border-l-4 border-green-500 dark:border-green-600 rounded-lg shadow-md flex items-center gap-4 hover:shadow-lg transition-all flex-wrap">
-						<div className="bg-green-500 text-white p-4 rounded-full">
-							<Wallet size={32} />
-						</div>
-						<div>
-							<h2 className="text-gray-700 dark:text-gray-200 font-semibold lg:text-lg  sm:text-sm">
-								No. of Budgets
-							</h2>
-							<h2 className="text-xl font-bold text-gray-900 dark:text-gray-100">
-								{budgetList.length}
 							</h2>
 						</div>
 					</div>
